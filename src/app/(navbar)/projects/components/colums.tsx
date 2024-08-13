3fbbbc6f2505580c31156725d09af16cd5e41028
Jarl Pallesen <jpa@naoma.dk>
2024-08-13 13:45:52 +0200

    spelling error in project table

--- src/app/(navbar)/projects/components/colums.tsx
+++ src/app/(navbar)/projects/components/colums.tsx
@@ -84,7 +84,7 @@ export const columns: ColumnDef<Project>[] = [
           variant="ghost"
           onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
         >
-          Totlat antal risici
+          Totale antal risici
           <ArrowUpDown className="ml-2 h-4 w-4" />
         </Button>
       );
