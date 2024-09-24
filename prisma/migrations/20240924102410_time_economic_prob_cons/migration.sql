1023744e1876bdbbcda9fc35853a4870d0e688a7
majapetrusic <maja.petrusic2501@gmail.com>
2024-09-24 12:25:22 +0200

    added migration for time and economic consequence and probability

---
+++ prisma/migrations/20240924102410_time_economic_prob_cons/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Risk" ADD COLUMN     "economicConsequence" INTEGER,
+ADD COLUMN     "economicProbability" INTEGER,
+ADD COLUMN     "timeConsequence" INTEGER,
+ADD COLUMN     "timeProbability" INTEGER;
