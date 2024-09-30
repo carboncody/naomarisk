ed3d7e8e1ab957dc54906e097d0729e0d7dd13b0
Jarl Pallesen <jpa@naoma.dk>
2024-09-30 17:57:41 +0200

    Delete and edit comments on risks

--- src/app/(navbar)/projects/[id]/risk/[rid]/components/comments/index.tsx
+++ src/app/(navbar)/projects/[id]/risk/[rid]/components/comments/index.tsx
@@ -1,35 +1,115 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from '@components/ui/alert-dialog';
 import type { Comment } from '@models';
 import { DropdownAnimation } from '@styles/animation';
-import dayjs from 'dayjs';
+import axios from 'axios';
 import { useState } from 'react';
+import toast from 'react-hot-toast';
+import { Comment as CommentComponent } from './Comment';
 import { CreateComment } from './CreateComment';
 
 interface Props {
   riskId: string;
   comments: Comment[];
+  refetch: () => void;
 }
 
-export function Comments({ riskId, comments: originalComments }: Props) {
+export function Comments({
+  riskId,
+  comments: originalComments,
+  refetch,
+}: Props) {
   const [comments, setComments] = useState<Comment[]>(originalComments);
+  const [commentBeingDeletedId, setCommentBeingDeletedId] = useState<
+    string | null
+  >(null);
+  const [commentBeingEditedId, setCommentBeingEditedId] = useState<
+    string | null
+  >(null);
+
+  async function onEdit(commentId: string | null, content: string) {
+    if (!commentId) return;
+
+    try {
+      await axios.patch(`/api/comment/${commentId}`, { content });
+      toast.success('Kommentar opdateret!');
+      setComments(
+        comments.map((c) => (c.id === commentId ? { ...c, content } : c)),
+      );
+      setCommentBeingEditedId(null);
+      refetch();
+    } catch (error) {
+      toast.error('Error - something went wrong');
+    }
+  }
+
+  async function onDelete(commentId: string | null) {
+    if (!commentId) return;
+
+    try {
+      await axios.delete(`/api/comment/${commentId}`);
+      toast.success('Kommentar slettet!');
+      setComments(comments.filter((c) => c.id !== commentId));
+      setCommentBeingDeletedId(null);
+      refetch();
+    } catch (error) {
+      toast.error('Error - something went wrong');
+    }
+  }
 
   return (
     <div className="flex flex-col gap-2">
       {comments.map((comment) => (
-        <DropdownAnimation
-          isOpen={comments.map((c) => c.id).includes(comment.id)}
-          key={comment.id}
-          initial
-        >
-          <div className="flex w-full flex-col gap-2 rounded-lg border p-2 dark:border-zinc-700">
-            <div className="flex items-center gap-2 text-zinc-400 dark:text-zinc-400">
-              <span>{dayjs(comment.createdAt).format('DD MMM HH:mm')}</span> -
-              <span>{comment.author.fullName}</span>
-            </div>
-            <span>{comment.content}</span>
-          </div>
+        <DropdownAnimation isOpen={true} key={comment.id} initial>
+          <CommentComponent
+            comment={comment}
+            commentBeingEditedId={commentBeingEditedId}
+            onEdit={onEdit}
+            setCommentBeingDeletedId={setCommentBeingDeletedId}
+            setCommentBeingEditedId={setCommentBeingEditedId}
+          />
         </DropdownAnimation>
       ))}
-      <CreateComment riskId={riskId} setComments={setComments} />
+      <CreateComment
+        riskId={riskId}
+        setComments={setComments}
+        refetch={refetch}
+      />
+      <AlertDialog
+        open={!!commentBeingDeletedId}
+        onOpenChange={(isOpen) =>
+          setCommentBeingDeletedId(isOpen ? commentBeingDeletedId : null)
+        }
+      >
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Er du helt sikker?</AlertDialogTitle>
+            <AlertDialogDescription>
+              Denne handling kan ikke fortrydes. Dette vil permanent slette
+              kommentaren.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel onClick={() => setCommentBeingDeletedId(null)}>
+              Annuller
+            </AlertDialogCancel>
+            <AlertDialogAction
+              className="bg-red-500 text-white hover:bg-red-700"
+              onClick={() => onDelete(commentBeingDeletedId)}
+            >
+              Slet kommentar
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </div>
   );
 }
